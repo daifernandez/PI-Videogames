effab00a8274214a854e56990ccc2757c34add5b
Daiana Fernandez <dtama.fernandez@gmail.com>
2022-12-19 11:42:45 -0800

    PI-Videogames

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 // Importar todos los routers;
 const videogamesRouter = require("./videogamesRouter.js");
-const genresRouter = require("./genresRouter.js");
 const videogameIDRouter = require("./videogameIDRouter.js");
+const genresRouter = require("./genresRouter.js");
 
 const router = Router();
 
