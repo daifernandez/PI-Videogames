bdd8843378ed4271f257000495d95ab85a1af8ee
Daiana Fernandez <dtama.fernandez@gmail.com>
2022-12-22 12:05:28 -0800

    PI-Videogames

--- api/src/controllers/genresControllers.js
+++ api/src/controllers/genresControllers.js
@@ -1,12 +1,16 @@
 const { Genre } = require("../db");
+const {get_genres}=  require("../helpers/genresHelpers.js")
 
 const get_genres = async (req, res) => {
-  // const genres = await Genre.findAll();
-  // if (genres.length > 0) {
-  //   res.send(200).send(genres);
-  // } else {
-  //   const apiGenres = await axios.get("https://api.rawg.io/api/genres");
-  // }
+  const genres = await Genre.findAll();
+  if (genres.length > 0) {
+    res.send(200).send(genres);
+  } else {
+   
+  }
+
 };
 
 module.exports = { get_genres };
+
+
