2dfb5df0531beedb320b8185d4aad80888a95155
Daiana Fernandez <dtama.fernandez@gmail.com>
2022-12-24 16:12:42 -0800

    PI-Videogames

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Styles/NavBar.css";
+import Logo from "../img/Icon.jpeg";
 
 export default function NavBar() {
   return (
     <div className="contenedor">
-      <p>Icono y Nombre de pagina</p>
+      <div className="img">
+        <img id="logo" src={Logo} className="img" alt="" />
+      </div>
       <NavLink to="/createVideogame">
         <button>Create Videogame</button>
       </NavLink>
