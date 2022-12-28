bd3b3a181bca1e324af9e989422d735f9e43d1cd
Daiana Fernandez <dtama.fernandez@gmail.com>
2022-12-27 16:37:12 -0800

    PI-Videogames

--- client/src/components/CreateVideogame.jsx
+++ client/src/components/CreateVideogame.jsx
@@ -18,6 +18,7 @@ export default function CreateVideogame() {
     description: "",
     rating: "",
     released: "",
+    image: "",
     genres: [],
     platforms: [],
   });
@@ -31,10 +32,20 @@ export default function CreateVideogame() {
       description: "",
       rating: "",
       released: "",
+      image: "",
       genres: [],
       platforms: [],
     });
     redirection.push("/home");
+  };
+
+  const handleChange = (e) => {
+    const property = e.target.name;
+    const value = e.target.value;
+    setForm({
+      ...form,
+      [property]: value,
+    });
   };
 
   const handleSelect = (e) => {
@@ -61,19 +72,43 @@ export default function CreateVideogame() {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Name:</label>
-          <input type="text" />
+          <input
+            type="text"
+            key="name"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Description:</label>
-          <input type="text" />
+          <input
+            type="text"
+            key="description"
+            name="description"
+            value={form.description}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Released:</label>
-          <input type="date" />
+          <input
+            type="date"
+            key="released"
+            name="released"
+            value={form.released}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Rating:</label>
-          <input type="range" />
+          <input
+            type="range"
+            key="rating"
+            name="rating"
+            value={form.rating}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Genres:</label>
@@ -113,7 +148,13 @@ export default function CreateVideogame() {
         </div>
         <div>
           <label>Image:</label>
-          <input type="file" />
+          <input
+            type="file"
+            key="image"
+            name="image"
+            value={form.image}
+            onChange={handleChange}
+          />
         </div>
         <button key="submit" type="submit" value="submit">
           Create Videogame
