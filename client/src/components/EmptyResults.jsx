062d80087f5e8224930072ff73f1e89808f97fa8
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-01-02 14:30:15 -0800

    PI-Videogames

---
+++ client/src/components/EmptyResults.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EmptyResults() {
+  return (
+    <div>
+       <p>No pudimos encontrar videojuegos para los filtros activos.</p>
+    </div>
+  );
+}
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,8 @@ import Search from "./Search";
 import Filter from "./Filters";
 import Cards from "./Cards";
 import Paginado from "./Paginado";
+import EmptyResults from "./EmptyResults";
+import Loading from "./Loading";
 import { getvideogames } from "../Redux/actions";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,28 +13,38 @@ import { useDispatch, useSelector } from "react-redux";
 export default function Home() {
   const dispatch = useDispatch();
   const videogames = useSelector((state) => state.videogamesOnScreen);
+  const fetchedVideogames = useSelector((state) => state.videogames.length > 0);
 
   useEffect(() => {
     dispatch(getvideogames());
   }, [dispatch]);
 
-  if (videogames.length) {
-    return (
-      <div>
-        <NavBar />
-        <Search />
-        <Filter />
-        <Cards key="videogames-cards" videogames={videogames} />
-        <Paginado />
-      </div>
-    );
+  if (fetchedVideogames) {
+    if (videogames.length) {
+      return (
+        <div>
+          <NavBar />
+          <Search />
+          <Filter />
+          <Cards key="videogames-cards" videogames={videogames} />
+          <Paginado />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <NavBar />
+          <Search />
+          <Filter />
+          <EmptyResults />
+        </div>
+      );
+    }
   } else {
     return (
       <div>
         <NavBar />
-        <Search />
-        <Filter />
-        <h1>Loading</h1>
+        <Loading />
       </div>
     );
   }
