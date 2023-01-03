e5a5a64d6efe9205f6b665c2b1a267ff7d78c54e
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-01-03 14:04:23 -0800

    PI-Videogames

--- client/src/components/CreateVideogame.jsx
+++ client/src/components/CreateVideogame.jsx
@@ -3,7 +3,8 @@ import { NavLink, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { postVideogame, getGenres } from "../Redux/actions";
-import "./Styles/NavBar.css";
+import "./Styles/CreateVideogame.css";
+import "./Styles/Button.css";
 
 const platforms = [
   "Game Boy Advance",
@@ -123,143 +124,158 @@ export default function CreateVideogame() {
   };
 
   return (
-    <div>
-      <NavLink exact to="/home">
-        <svg
-          className="home-button"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="black"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-          />
-        </svg>
-      </NavLink>
-      <h1>Formulario de creacion de videojuego</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            key="name"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-          />
-          {error.name && <p>{error.name}</p>}
-        </div>
-        <div>
-          <label>Description:</label>
-          <input
-            type="text"
-            key="description"
-            name="description"
-            value={form.description}
-            onChange={handleChange}
-          />
-          {error.description && <p>{error.description}</p>}
-        </div>
-        <div>
-          <label>Released:</label>
-          <input
-            type="date"
-            key="released"
-            name="released"
-            value={form.released}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Rating:</label>
-          <input
-            type="number"
-            step="0.1"
-            min="0"
-            max="5"
-            key="rating"
-            name="rating"
-            value={form.rating}
-            onChange={handleChange}
-          />
-          {error.rating && <p>{error.rating}</p>}
-        </div>
-        <div>
-          <fieldset>
-            <legend>Choose Genres:</legend>
-            <select
-              key="genreName"
-              name="genreName"
-              id=""
-              onChange={handleSelect}
-            >
-              {genres.map((genre) => (
-                <option>{genre.name}</option>
-              ))}
-            </select>
-            <div>
-              <>
-                {form.genres.map((genre) => (
-                  <ul key={genre.name} value={genre.name}>
-                    {genre}
-                    <button
-                      type="button"
-                      value={genre}
-                      onClick={(e) => handleDeleteGenre(e)}
-                    >
-                      x
-                    </button>
-                  </ul>
+    <div className="contenedor-create">
+      <div className="contendor-titulo">
+        <NavLink exact to="/home">
+          <svg
+            className="home-button"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="black"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </NavLink>{" "}
+        <h1>You cant add your own Videogame!!</h1>
+      </div>
+      <div className="contenedor-create">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className="input-title">Name:</label>
+            <input
+              className="input-create"
+              type="text"
+              key="name"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            {error.name && <p className="input-validation">{error.name}</p>}
+          </div>
+          <div>
+            <label className="input-title">Description:</label>
+            <input
+              className="input-create"
+              type="text"
+              key="description"
+              name="description"
+              value={form.description}
+              onChange={handleChange}
+            />
+            {error.description && (
+              <p className="input-validation">{error.description}</p>
+            )}
+          </div>
+          <div>
+            <label className="input-title">Released:</label>
+            <input
+              className="input-create"
+              type="date"
+              key="released"
+              name="released"
+              value={form.released}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label className="input-title">Rating:</label>
+            <input
+              className="input-create"
+              type="number"
+              step="0.1"
+              min="0"
+              max="5"
+              key="rating"
+              name="rating"
+              value={form.rating}
+              onChange={handleChange}
+            />
+            {error.rating && <p className="input-validation">{error.rating}</p>}
+          </div>
+          <div>
+            <fieldset>
+              <legend className="input-title">Choose Genres:</legend>
+              <select
+                className="input-create"
+                key="genreName"
+                name="genreName"
+                id=""
+                onChange={handleSelect}
+              >
+                {genres.map((genre) => (
+                  <option>{genre.name}</option>
                 ))}
-              </>
-            </div>
-          </fieldset>
-        </div>
+              </select>
+              <div>
+                <>
+                  {form.genres.map((genre) => (
+                    <button
+                    >
+                    <ul key={genre.name} value={genre.name}>
+                      {genre}
+                      <button
+                        type="button"
+                        value={genre}
+                        onClick={(e) => handleDeleteGenre(e)}
+                      >
+                        x
+                      </button>
+                    </ul>
+                    </button>
+                  ))}
+                </>
+              </div>
+            </fieldset>
+          </div>
 
-        <br />
-
-        <fieldset>
-          <legend>Choose Platforms:</legend>
           <br />
-          {platforms.map((platform) => (
-            <label>
-              <input
-                type="checkbox"
-                name={platform}
-                value={platform}
-                onChange={handleSelectPlatform}
-              />
-              {platform}
-              <br />
-              {error.platform && <p>{error.platform}</p>}
-            </label>
-          ))}
-        </fieldset>
 
-        <br />
-        <div>
-          <label>Image:</label>
-          <input
-            type="file"
-            key="image"
-            name="image"
-            value={form.image}
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          key="submit"
-          type="submit"
-          value="submit"
-          disabled={!form.name || !form.description}
-        >
-          Create Videogame
-        </button>
-      </form>
+          <fieldset>
+            <legend className="input-title">Choose Platforms:</legend>
+            <br />
+            {platforms.map((platform) => (
+              <label>
+                <input
+                  type="checkbox"
+                  name={platform}
+                  value={platform}
+                  onChange={handleSelectPlatform}
+                />
+                {platform}
+                <br />
+                {error.platform && <p>{error.platform}</p>}
+              </label>
+            ))}
+          </fieldset>
+
+          <br />
+          <div>
+            <label className="input-title">Image:</label>
+            <input
+              type="file"
+              key="image"
+              name="image"
+              value={form.image}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className="custom-button"
+            key="submit"
+            type="submit"
+            value="submit"
+            disabled={!form.name || !form.description}
+          >
+            Create Videogame
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
