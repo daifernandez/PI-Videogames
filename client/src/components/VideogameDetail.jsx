df9cbdfe0aeac74f9db9edf3a6943c78d040b1d5
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-01-03 14:44:02 -0800

    PI-Videogames

--- client/src/components/VideogameDetail.jsx
+++ client/src/components/VideogameDetail.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getVideogameDetail } from "../Redux/actions";
 import "./Styles/VideogameDetail.css";
-import NavBar from "./NavBar";
 
 export default function Detail() {
   const { id } = useParams();
@@ -18,7 +17,6 @@ export default function Detail() {
   if (detail) {
     return (
       <div>
-        <NavBar />
         <div className="detail-container">
           <img
             className="detail-main-image"
@@ -28,11 +26,10 @@ export default function Detail() {
           <div className="detail-content">
             <div className="detail-text-info">
               <h1>{detail.name}</h1>
-              <h4>{detail.rating}</h4>
-              <h6>Released: {detail.released}</h6>
-
-              <div dangerouslySetInnerHTML={{ __html: detail.description }} />
               <h5>{detail.genres.join(" - ")}</h5>
+              <h4 className="star-rating">⭐️ {detail.rating}</h4>
+              <h6>Released: {detail.released}</h6>
+              <div dangerouslySetInnerHTML={{ __html: detail.description }} />
               <h4>{detail.platforms.join(", ")}</h4>
             </div>
           </div>
