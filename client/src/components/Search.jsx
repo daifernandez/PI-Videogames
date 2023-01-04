cbc5cbcc94260b758b968c056dd5fd1d0fbdd21f
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-01-04 11:24:09 -0800

    PI-Videogames

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -36,7 +36,7 @@ export default function Search() {
         </button>
       </div>
       <NavLink to="/createVideogame">
-        <button className="custom-button">
+        <button className="custom-buttonIcon">
           <div className="icon-text">
             <svg
               xmlns="http://www.w3.org/2000/svg"
