cef1930b5eb5ebdf043bda6245f19d13356643d6
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-01-17 09:59:38 -0800

    PI-Videogames

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-videogames-backend.vercel.app"
+    "https://pi-videogames-one-lake.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
