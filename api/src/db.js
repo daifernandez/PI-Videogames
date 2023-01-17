292ba30108f39cc5fc1466fd8c097558612749dd
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-01-17 09:25:25 -0800

    PI-Videogames

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+const pg = require("pg");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
@@ -10,6 +11,7 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectModule: pg,
   }
 );
 const basename = path.basename(__filename);
