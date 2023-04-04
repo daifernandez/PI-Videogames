eb16ecf0f61b6983ede79e88a81a0a3254d0041f
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-04-03 19:48:27 -0700

    social links arreglo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-section">
-        <span className="footer-text">Individual Project - Videogame App</span>{" "}
+        <span className="footer-text">Individual Project - Videogame App</span>
         <span className="footer-text">Henry Bootcamp Projects - 2022</span>
       </div>
       <div className="footer-social">
